302a8929b948befc11bc17f14877412f8c7cf045
Abdal Hussain <abdalhussain5790@gmail.com>
2025-03-28 15:01:08 +0500

    Create a structure for Number Guessing Game

---
+++ Javascript Program/numberGuessingGame.js
@@ -0,0 +1,11 @@
+let input = prompt("Enter a number between 1-10");
+
+if(Number(input)){
+    if(input >= 0 && input <=10){
+        
+    }else{
+        alert("Please enter number between 1-10")
+    }
+}else{
+    alert("Please enter a digit between 1-10");
+}
